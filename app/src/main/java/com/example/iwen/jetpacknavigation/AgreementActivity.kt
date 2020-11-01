9eb4ce2863d98a5cf25e52df2f4564da4b564d49
iwenLjr <1816807688@qq.com>
2020-11-01 23:55:22 +0800

    跳转到其他Activity

---
+++ app/src/main/java/com/example/iwen/jetpacknavigation/AgreementActivity.kt
@@ -0,0 +1,17 @@
+package com.example.iwen.jetpacknavigation
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * author : Iwen大大怪
+ * create : 2020/11/1 23:43
+ */
+class AgreementActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agreement)
+    }
+}
--- app/src/main/java/com/example/iwen/jetpacknavigation/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/iwen/jetpacknavigation/fragment/LoginFragment.kt
@@ -1,7 +1,10 @@
 package com.example.iwen.jetpacknavigation.fragment
 
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.example.iwen.jetpacknavigation.R
 import com.example.iwen.jetpacknavigation.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_login.view.*
 
 /**
  * 登录的Fragment
@@ -11,5 +14,24 @@ import com.example.iwen.jetpacknavigation.base.BaseFragment
 class LoginFragment: BaseFragment() {
     override fun getLayoutResId(): Int {
         return R.layout.fragment_login
+    }
+
+    override fun initView(rootView: View) {
+        super.initView(rootView)
+        rootView.toRegisterPage.setOnClickListener {
+            // 跳转到注册界面
+            findNavController().navigate(R.id.to_register_fragment)
+        }
+
+        rootView.toForgetPage.setOnClickListener {
+            // 跳转到找回密码界面
+            findNavController().navigate(R.id.to_forget_fragment)
+        }
+
+        rootView.toAgreementPage.setOnClickListener {
+            // 跳转到找回密码界面
+            findNavController().navigate(R.id.to_agreement_page)
+        }
+
     }
 }
